c553c75fc58d8a3d3dbf6297e510241410977961
Cecilia <ysycecilia@gmail.com>
2021-06-18 04:07:35 +0000

    create go-to-top btn

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -106,6 +106,14 @@ $(document).ready(function() {
   });
 });
 
+$(document).ready(function() {
+  $(".toTopBtn").click(function() {
+    console.log("hhhhhhhh");
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+  });
+});
 
 const loadtweets = function() {
   $.ajax("/tweets", {
