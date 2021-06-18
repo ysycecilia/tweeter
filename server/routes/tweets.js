ef4f85a1af524e2dde116d560a528cbfbd89dc24
Cecilia <ysycecilia@gmail.com>
2021-06-18 04:15:32 +0000

    cleanup

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,6 +1,7 @@
+/* eslint-disable camelcase */
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
+const userHelper    = require("../lib/util/user-helper");
 
 const express       = require('express');
 const tweetsRoutes  = express.Router();
@@ -43,4 +44,4 @@ module.exports = function(DataHelpers) {
 
   return tweetsRoutes;
 
-}
+};
